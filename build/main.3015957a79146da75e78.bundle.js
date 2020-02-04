bf88078a36e51bdc70f0183741b4077468a4f205
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-02-03 21:51:15 -0500

    Refractors sass file structure

---
+++ build/main.3015957a79146da75e78.bundle.js
@@ -0,0 +1,9 @@
+!function(e){var r={};function _(n){if(r[n])return r[n].exports;var s=r[n]={i:n,l:!1,exports:{}};return e[n].call(s.exports,s,s.exports,_),s.l=!0,s.exports}_.m=e,_.c=r,_.d=function(e,r,n){_.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:n})},_.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},_.t=function(e,r){if(1&r&&(e=_(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(_.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var s in e)_.d(n,s,function(r){return e[r]}.bind(null,s));return n},_.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return _.d(r,"a",r),r},_.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},_.p="",_(_.s="./src/index.js")}({"./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sass/main.scss */ "./src/sass/main.scss");\n/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_sass_main_scss__WEBPACK_IMPORTED_MODULE_0__);\n\n\n//# sourceURL=webpack:///./src/index.js?')},"./src/sass/main.scss":
+/*!****************************!*\
+  !*** ./src/sass/main.scss ***!
+  \****************************/
+/*! no static exports found */function(module,exports,__webpack_require__){eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/sass/main.scss?")}});
